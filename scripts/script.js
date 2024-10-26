f83fc82ff4c8374c1b455ea90d2f24250b85ef43
Sana <Lenovo@DESKTOP-I4OODFD>
2024-10-27 00:44:08 +0200

    Create - reachEnd func

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,51 @@
 var startBtn = document.getElementById("start");
 startBtn.addEventListener("mouseover", start);
 
-var walls = document.querySelectorAll(".boundary");
-for (var i = 0; i < walls.length; i++) {
-  walls[i].addEventListener("mouseover", function () {
-    turnWallsRed(walls);
-  });
+function start() {
+  //   windows.location.reload();
+  var walls = document.querySelectorAll(".boundary");
+  preventTouchingWalls(walls);
+  preventCheating();
+  reachEnd();
+}
+function preventTouchingWalls(walls) {
+  for (var i = 0; i < walls.length; i++) {
+    walls[i].addEventListener("mouseover", function () {
+      turnWallsRed(walls);
+    });
+  }
 }
 
-function start() {}
+function preventCheating() {
+  var game_canvas = document.getElementById("game");
+  game_canvas.addEventListener("mouseleave", turnWallsRed);
+}
 function turnWallsRed(walls) {
+  var walls = document.querySelectorAll(".boundary");
   for (var i = 0; i < walls.length; i++) {
     walls[i].classList.add("highlighted");
   }
+  changeStatus(false);
 }
-function preventCheating() {
+function removeCheatingRestriction(walls) {
   var game_canvas = document.getElementById("game");
-  game_canvas.addEventListener("mouseleave", function () {
-    turnWallsRed(walls);
+
+  game_canvas.removeEventListener("mouseleave", turnWallsRed);
+}
+
+function reachEnd() {
+  var end = document.getElementById("end");
+  end.addEventListener("mouseover", function () {
+    changeStatus(true);
+    removeCheatingRestriction();
   });
+}
+
+function changeStatus(result) {
+  var status = document.getElementById("status");
+  if (result) {
+    status.innerHTML = "Congrats!! You Won!";
+  } else {
+    status.innerHTML = "Oops!! Try Again...";
+  }
 }
