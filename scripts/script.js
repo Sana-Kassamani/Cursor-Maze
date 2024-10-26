6a5a9235de7700811965ab153765221b173f4dce
Sana <Lenovo@DESKTOP-I4OODFD>
2024-10-26 23:29:46 +0200

    Create - preventCheating and preventTouchingWalls

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
 var startBtn = document.getElementById("start");
 startBtn.addEventListener("mouseover", start);
 
-function start() {
-  turnWallsRed();
+var walls = document.querySelectorAll(".boundary");
+for (var i = 0; i < walls.length; i++) {
+  walls[i].addEventListener("mouseover", function () {
+    turnWallsRed(walls);
+  });
 }
-function turnWallsRed() {
-  var walls = document.querySelectorAll(".boundary");
+
+function start() {}
+function turnWallsRed(walls) {
   for (var i = 0; i < walls.length; i++) {
-    walls[i].addEventListener("mouseover", function () {
-      for (var i = 0; i < walls.length; i++) {
-        walls[i].classList.add("highlighted");
-      }
-    });
+    walls[i].classList.add("highlighted");
   }
+}
+function preventCheating() {
+  var game_canvas = document.getElementById("game");
+  game_canvas.addEventListener("mouseleave", function () {
+    turnWallsRed(walls);
+  });
 }
